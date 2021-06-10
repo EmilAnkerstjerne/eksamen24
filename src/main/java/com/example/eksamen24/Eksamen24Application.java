d93f9be5445c6b121cac4def8ca782be22208cfb
Emil Ankerstjerne <emil.ankerstjerne@gmail.com>
2021-06-10 09:52:08 +0200

    first commit

---
+++ src/main/java/com/example/eksamen24/Eksamen24Application.java
@@ -0,0 +1,13 @@
+package com.example.eksamen24;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Eksamen24Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Eksamen24Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/eksamen24/Eksamen24ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eksamen24;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Eksamen24ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
